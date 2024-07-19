1c8cdea509f621764c36b1009baf1eaef62be510
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-07-19 23:50:38 +0300

    step 21

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-    const sortedValues = bubbleSort(inputValues)
+    const sortedValues = bubbleSort(inputValues);
     updateUI(sortedValues);
 }
 
@@ -16,8 +16,11 @@ const updateUI = (array = []) => {
 
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
-        for(let j = 0; j < array.length - 1; j++) {
-            console.log(array, array[j], array[j+1])
+        for (let j = 0; j < array.length - 1; j++) {
+            console.log(array, array[j], array[j + 1]);
+            if (array[j] > array[j + 1]) {
+
+            }
         }
 
     }
